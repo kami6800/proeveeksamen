f9f1be7026e19eff9b2bb46b5e00b1e8d5701b74
Kamilla Bilet Høst <kami6800@edu.campusvejle.dk>
2021-01-06 09:51:54 +0100

    finished add mood layout

--- src/pages/AddMood.vue
+++ src/pages/AddMood.vue
@@ -1,7 +1,18 @@
 <template>
-  <button>back</button>
+  <button>
+    <svg class="text-green-800 w-7 h-7 my-2">
+      <use xlink:href="#arrowBack" />
+    </svg>
+  </button>
   
-  <mood-emojis></mood-emojis>
+  <form>
+    <p class="mb-2">Hvordan har du det?</p>
+    <input class="w-full h-10 p-2 border" type="text"/>
+    <mood-emojis></mood-emojis>
+    <p class="mb-2">Beskrivelse</p>
+    <textarea class="w-full p-2 h-52 border" />
+    <button class="float-right px-4 py-2 mt-8 bg-green-800 text-white rounded" type="submit">Gem</button>
+  </form>
 </template>
 
 <script>
