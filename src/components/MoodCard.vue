a72a090a674150698823352db8d9b6e0f107f36b
Kamilla Bilet Høst <kami6800@edu.campusvejle.dk>
2021-01-06 11:20:11 +0100

    created timestamp to display time ago

--- src/components/MoodCard.vue
+++ src/components/MoodCard.vue
@@ -16,7 +16,53 @@ export default {
     },
     computed:{
         timeSince(){
-            return "hi";
+            let seconds = Math.floor((Date.now() - this.time) / 1000);
+            let interval = seconds/31536000;
+            if(interval>1){
+                interval = Math.floor(interval);
+                if(interval === 1)
+                    return interval + " year ago";
+                else
+                    return interval + " years ago";
+            }
+
+            interval = seconds / 2592000;
+            if(interval>1){
+                interval = Math.floor(interval);
+                if(interval === 1)
+                    return interval + " month ago";
+                else
+                    return interval + " months ago";
+            }
+
+            interval = seconds / 86400;
+            if(interval>1){
+                interval = Math.floor(interval);
+                if(interval === 1)
+                    return interval + " day ago";
+                else
+                    return interval + " days ago";
+            }
+
+            interval = seconds / 3600;
+            if(interval>1){
+                interval = Math.floor(interval);
+                if(interval === 1)
+                    return interval + " hour ago";
+                else
+                    return interval + " hours ago";
+            }
+
+            interval = seconds / 60;
+            if(interval>1){
+                interval = Math.floor(interval);
+                if(interval === 1)
+                    return interval + " minute ago";
+                else
+                    return interval + " minutes ago";
+            }
+            
+            return seconds + " seconds ago";
         }
     }
 }
--- src/store/index.js
+++ src/store/index.js
@@ -8,14 +8,14 @@ const store = createStore({
                 title:"test",
                 description:"ytest",
                 mood:4,
-                timestamp: new Date(2017, 11, 24, 10, 33, 30, 0)
+                timestamp: new Date(2021, 0, 6, 11, 19)
             },
             {
                 id:5,
                 title:"test33",
                 description:"ytest33",
                 mood:1,
-                timestamp: new Date(2018, 11, 24, 10, 33, 30, 0)
+                timestamp: new Date(2019, 11, 24, 10, 33, 30, 0)
             }]
         }
     },
