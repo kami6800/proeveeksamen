d13aaf22c93acf977db9904d6a62a6b2eb193386
Kamilla Bilet Høst <kami6800@edu.campusvejle.dk>
2021-01-07 09:09:51 +0100

    optimized routing, added url not found page

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,7 @@
+<template>
+<div class="text-center">
+    <p class="m-8 mb-16">Ugyldig adresse</p>
+    <router-link class="bg-green-800 text-white p-2 px-4 rounded transition-all duration-150
+                        hover:bg-green-600 hover:shadow-lg" to="/all">Tilbage til forsiden</router-link>
+    </div>
+</template>
--- src/router.js
+++ src/router.js
@@ -1,12 +1,15 @@
 import {createRouter, createWebHistory} from "vue-router";
 import AllMoods from "./pages/AllMoods";
 import AddMood from "./pages/AddMood";
+import NotFound from "./pages/NotFound";
 
 const router = createRouter({
     history: createWebHistory(),
     routes:[
+        {path: "/", redirect: "/all"},
         {path:"/all", component:AllMoods},
-        {path: "/add", component:AddMood}
+        {path: "/add", component:AddMood},
+        {path: "/:notFound(.*)", component:NotFound}
     ]
 });
 
